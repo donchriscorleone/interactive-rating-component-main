4cd65ad2ab0568a27f97e20369697c2842d15fa7
Christopher II Lajom <christopheriilajom@gmail.com>
2022-03-20 14:46:41 +0800

    Added subText dynamic.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const card = document.querySelector('div.card');
 const modal = document.querySelector('div.dialog');
 const submitBtn = document.querySelector('button.btn--submit')
 
+const subText = document.querySelector('p.card--icon__sub')
+
 for (let i = 0; i < buttons.length; i++) {
     buttons.item(i).addEventListener('click', (e) => {
         rate = i + 1;
@@ -15,5 +17,6 @@ submitBtn.addEventListener('click', (e) => {
     if (rate > 0) {
         modal.style.display = 'grid';
         card.style.display = 'none'
+        subText.innerHTML = `You selected ${rate} out of 5`
     }
 })
